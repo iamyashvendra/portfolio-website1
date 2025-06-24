4169cf0378a1f542e622d6ebfb5409e25e2ebe65
iamyashvendra <yashvender720@gmail.com>
2025-06-25 02:02:08 +0530

    fix: rebuild and deploy fresh dist

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolio-website/',
+  base: '/portfolio-website1/',
   plugins: [react()],
 })
